367da4a4c3b36681fad4db366f745eb001c645f2
dev-tes <tesleem.amuda@gmail.com>
2021-09-08 00:34:38 +0100

    Create SignupVC

---
+++ CollaborationTest/CollaborationTest/SignupViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignupViewController.swift
+//  CollaborationTest
+//
+//  Created by  Decagon on 08/09/2021.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+
+}
